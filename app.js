06ffb0b42ccbead2f66f4436c0eb402a91df2706
Kunal Gaurav <kunalgaurav0405@gmail.com>
2020-07-22 15:01:38 +0530

    added controllers

--- app.js
+++ app.js
@@ -3,21 +3,11 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieparser from "cookie-parser";
 import bodyparser from "body-parser";
-import { userRouter } from "./router";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import routes from "./routes";
 const app = express();
-
-const handleHome = (req, res) => {
-  res.send("Hello from home");
-};
-
-const handleProfile = (req, res) => {
-  res.send("Its new page");
-};
-
-const betweenHome = (req, res, next) => {
-  console.log("Hey betwwen");
-  next();
-};
 
 app.use(cookieparser());
 app.use(bodyparser.json());
@@ -25,9 +15,8 @@ app.use(bodyparser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-// app.get("/", handleHome);
-app.get("/profile", handleProfile);
-
-app.use("/", userRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
